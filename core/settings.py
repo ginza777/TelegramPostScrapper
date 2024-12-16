92b12f2c327e3f71f3c857d6ec554eefd555c672
admin <muhiddinovsherzamon@icloud.com>
2024-12-16 23:48:19 +0500

    host

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["185.203.216.253","ads.zamonsher.icu","127.0.0.1"]
 
 # Application definition
 
